93f435b4f66a322d349085365e2c8734923c940e
Marlon <m.de.la.cruz@hotmail.com>
2019-04-10 20:27:09 +0000

    set u p the WS Server

--- chatty_WebSocket_Server/server.js
+++ chatty_WebSocket_Server/server.js
@@ -16,10 +16,15 @@ const server = express()
 // Create the WebSockets server
 const wss = new SocketServer({ server });
 
+
+
 // Set up a callback that will run when a client connects to the server
 // When a client connects they are assigned a socket, represented by
 // the ws parameter in the callback.
 wss.on('connection', (ws) => {
+
+
+
   console.log('Client connected');
 
   // Set up a callback for when a client closes the socket. This usually means they closed their browser.
