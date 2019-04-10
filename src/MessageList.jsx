2d238f7ee3fc41fa524d3763e5874b662c5dc8cb
Marlon <m.de.la.cruz@hotmail.com>
2019-04-10 23:43:33 +0000

    key=ID added to return msg

---
+++ src/MessageList.jsx
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+import Message from './Message.jsx';
+
+class MessageList extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+
+
+  render() {
+    return (
+      <main className="messages">
+           {this.props.messages.map(message =>
+              <Message  content={message.content}
+                        username={message.username}
+                        key={message.id}/> /** every msg must have a key */
+           )}
+      </main>
+    )
+  }
+}
+
+export default MessageList;
+
