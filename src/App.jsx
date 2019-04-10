93f96fa0151ebf739fed08090d997d1a54ae7678
Marlon <m.de.la.cruz@hotmail.com>
2019-04-10 19:05:43 +0000

    added class / when user adds msg gets updated to app then children/ render returned html MsgList & chatBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,81 @@
 import React, {Component} from 'react';
+import ChatBar from './ChatBar.jsx';
+import MessagesList from './MessageList.jsx';
+import uuid from "uuid";
+
+
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currentUser: {name: "Anonymous"}, // optional. if currentUser is not defined, it means the user is Anonymous
+      messages: [
+        {
+          id: uuid(),
+          username: "Bob",
+          content: "Has anyone seen my marbles?",
+        },
+        {
+          id: uuid(),
+          username: "Anonymous",
+          content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
+        }
+      ]
+    }
+  }
+
+  componentDidMount() {
+    console.log("componentDidMount <App />");
+    setTimeout(() => {
+      console.log("Simulating incoming message");
+      // Add a new message to the list of messages in the data store
+      const newMessage = {id: 3, username: "Michelle", content: "Hello there!"};
+      const messages = this.state.messages.concat(newMessage)
+      // Update the state of the app component.
+      // Calling setState will trigger a call to render() in App and all child components.
+      this.setState({messages: messages})
+    }, 3000);
+  }
+
+
+
+  onChatbarSubmit = (newMessage) => {                 // the newMsg is now a str of the msg
+
+    const StructureMessage =
+    {
+      id: uuid(),                                     // this should  be uuid() each msg should have a unique #
+      username: this.state.currentUser.name,
+      content: newMessage,
+    }
+
+    const oldMessages = this.state.messages
+    this.setState({messages: [...oldMessages, StructureMessage] })
+
+  };
+
+
+
+
   render() {
     return (
-      <h1>Hello React :)</h1>
+    <div>
+      <nav className="navbar">
+        <a href="/" className="navbar-brand">Chatty</a>
+      </nav>
+      {/**  the below line is refering to MessageList.jsx using "import" above*/}
+      <MessagesList messages={this.state.messages} submitEvent={this.onChatbarMessageSubmit}/>
+      {/**  the below line is refering to ChatBar.jsx /and/ sned it props "currentUser"*/}
+      <ChatBar currentUser={this.state.currentUser} onChatbarSubmit={this.onChatbarSubmit}/>
+
+    </div>
     );
   }
+
+
+
 }
+
 export default App;
