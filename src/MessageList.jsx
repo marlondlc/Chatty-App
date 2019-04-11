c4427cd4906c6f38a970bb076379124f295b01c2
Marlon <m.de.la.cruz@hotmail.com>
2019-04-11 15:02:07 +0000

    deleted import for notification/ not needed at time

--- src/MessageList.jsx
+++ src/MessageList.jsx
@@ -2,11 +2,6 @@ import React, {Component} from 'react';
 import Message from './Message.jsx';
 
 class MessageList extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-
 
   render() {
     return (
