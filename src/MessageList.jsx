601f06262a4a4ec84ec9166387753924b9e8019b
Marlon <m.de.la.cruz@hotmail.com>
2019-04-12 15:05:25 +0000

    handling the nofitication with an if statement

--- src/MessageList.jsx
+++ src/MessageList.jsx
@@ -1,17 +1,34 @@
 import React, {Component} from 'react';
 import Message from './Message.jsx';
+import Notification from './Notification.jsx';
 
 class MessageList extends Component {
 
+
   render() {
+
+
+    let messageList = this.props.messages.map(message => {
+      if(message.type == 'incomingMessage') {
+        return <Message  content={message.content}
+                  username={message.username}
+                  key={message.id}/> /** every msg must have a key */
+    } else {
+        return <Notification  content={message.content}
+        key={message.id}/>
+    }
+    })
+
+
+
+
     return (
-      <main className="messages">
-           {this.props.messages.map(message =>
-              <Message  content={message.content}
-                        username={message.username}
-                        key={message.id}/> /** every msg must have a key */
-           )}
-      </main>
+      <div>
+      <p>{this.props.username}</p>
+        <main className="messages">
+        {messageList}
+        </main>
+      </div>
 
     )
   }
